06909ff7dd0d68dc98326b21dd52aaedb82a2868
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-03-13 10:34:31 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
+    printf("char: %u\n", sizeof('abc'));
+    printf("short: %u\n", sizeof(4.2));
+    printf("int: %u\n", sizeof(35));
+    printf("unsigned int: %u\n", sizeof(-1));
+    printf("long: %u\n", sizeof(1.23));
+    printf("long long: %u\n", sizeof(1.2345));
+    printf("float: %u\n", sizeof(3.58243));
 
     return 0;
 }
