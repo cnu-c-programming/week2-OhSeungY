c7228f4418c570cdb488fe89ce066cacecffd203
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-03-19 12:10:08 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,22 @@
 
 int main()
 {
+    int x;
+    int isPrime = 1;
 
+    scanf("%d", &x);
+    for(int i=2; i<x; i++){
+        if(x % i == 0){
+            isPrime = 0;
+        }
+    }
+    if(isPrime == 1){
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    
     return 0;
 }
 
