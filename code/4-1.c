6a1627b57daf703e744a868637d5b33cf9ddab31
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-03-19 11:58:57 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,30 @@
 
 int main()
 {
+    int x;
+    int y;
+    char operator;
+    int result;
 
+    scanf("%d %d %c", &x, &y, &operator);
+
+    switch(operator){
+        case '+' :
+            result = x + y;
+            break;
+        case '-' :
+            result = x - y;
+            break;
+        case '*' :
+            result = x * y;
+            break;
+        case '/' :
+            result = x / y;
+            break;    
+    }
+
+    printf("%d", result);
+    
     return 0;
 }
 
